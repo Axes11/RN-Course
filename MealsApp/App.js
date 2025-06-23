8736d30bf9f8ea0c51c4101ed6ed5a451eec5173
Ivan <123070177+Axes11@users.noreply.github.com>
2025-06-23 23:10:53 +0200

    start meal app

---
+++ MealsApp/App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+
+export default function App() {
+	return (
+		<>
+			<StatusBar style='light'></StatusBar>
+			<CategoriesScreen />
+		</>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {},
+});
